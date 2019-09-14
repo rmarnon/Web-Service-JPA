be25ee9f26dc9d82eb0a59d54be149f53cec27a7
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-14 11:56:23 -0300

    Server port change to 9080

--- src/main/java/com/Rodrigo/webserviceJPA/WebServiceJpaApplication.java
+++ src/main/java/com/Rodrigo/webserviceJPA/WebServiceJpaApplication.java
@@ -3,6 +3,7 @@ package com.Rodrigo.webserviceJPA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Classe principal -> Entry Point da Aplicacao
 @SpringBootApplication
 public class WebServiceJpaApplication {
 
