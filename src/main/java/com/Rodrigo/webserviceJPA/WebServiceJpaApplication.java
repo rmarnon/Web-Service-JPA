1c8ad11fee83b5b5822fc796651849fec2473aed
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-14 10:52:00 -0300

    Config Sprin Web JPA

---
+++ src/main/java/com/Rodrigo/webserviceJPA/WebServiceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.Rodrigo.webserviceJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceJpaApplication.class, args);
+	}
+
+}
