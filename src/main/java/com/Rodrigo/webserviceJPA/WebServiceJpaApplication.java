31484a6fd91329b56c65c615de4eacdea06a32ff
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-16 17:43:35 -0300

    H2 and JPA update 1.2

--- src/main/java/com/Rodrigo/webserviceJPA/WebServiceJpaApplication.java
+++ src/main/java/com/Rodrigo/webserviceJPA/WebServiceJpaApplication.java
@@ -3,12 +3,14 @@ package com.Rodrigo.webserviceJPA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Classe principal -> Entry Point da Aplicacao
+/*
+ * Classe principal -> Entry Point da Aplicacao
+ */
+
 @SpringBootApplication
 public class WebServiceJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceJpaApplication.class, args);
 	}
-
 }
